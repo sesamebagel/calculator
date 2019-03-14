ea4f8a0879b182d3ce73a254af762a76a1ccdedc
sesamebagel <34522551+sesamebagel@users.noreply.github.com>
2019-03-14 16:17:18 -0400

    Checking subtract method

--- src/testingstuff/TestCalculator.java
+++ src/testingstuff/TestCalculator.java
@@ -5,6 +5,7 @@ import org.testng.asserts.SoftAssert;
 public class TestCalculator {
    public static void main(String[] args) {
       Assert.assertEquals(Calculator.add(10,20), 30);
+      Assert.assertEquals(Calculator.subtract(10,20), -10);
    }
 
 }
