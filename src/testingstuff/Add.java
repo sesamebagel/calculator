84ba185999cfccb7703a2694477ed832c8b328c6
sesamebagel <34522551+sesamebagel@users.noreply.github.com>
2019-03-14 19:21:22 -0400

    Create Add.java

---
+++ src/testingstuff/Add.java
@@ -0,0 +1,11 @@
+public class Add {
+   public static int addTwoNums(int x, int y, int z) {
+      return x + y + z;
+   }
+
+   public static void main(String[] args) {
+      System.out.println(Add.addTwoNums(3,5, 6));
+      System.out.println(Add.addTwoNums(3,7, 8));
+      System.out.println(Add.addTwoNums(3,7, 9));
+   }
+}
