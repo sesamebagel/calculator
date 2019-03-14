5b4ab6ede0762646dae739bfb35b936df37d1c9b
sesamebagel <34522551+sesamebagel@users.noreply.github.com>
2019-03-14 16:44:28 -0400

    Update TestCalculator.java

--- src/testingstuff/TestCalculator.java
+++ src/testingstuff/TestCalculator.java
@@ -1,11 +1,10 @@
 package testingstuff;
 
-import org.testng.asserts.SoftAssert;
+import org.testng.asserts.Assert;
 
 public class TestCalculator {
    public static void main(String[] args) {
       Assert.assertEquals(Calculator.add(10,20), 30);
-      Assert.assertEquals(Calculator.subtract(10,20), -10);
    }
 
 }
