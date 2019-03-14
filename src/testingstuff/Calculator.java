0e348be4b721bbc9f6e05b3d65f448388efa661c
sesamebagel <34522551+sesamebagel@users.noreply.github.com>
2019-03-14 15:36:14 -0400

    added divide method

--- src/testingstuff/Calculator.java
+++ src/testingstuff/Calculator.java
@@ -10,6 +10,10 @@ public class Calculator {
    }
 
    public static int multiply(int x, int y) {
-      return x*y;
+      return x * y;
+   }
+   
+   public static int divide(int x, int y) {
+      return x / y;
    }
 }
